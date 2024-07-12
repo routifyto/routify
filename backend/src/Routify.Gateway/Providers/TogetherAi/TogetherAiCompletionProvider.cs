562f5d5e7159c5785a022f3a31a8ac424902203b
Hakan Shehu <hakanshehu15@gmail.com>
2024-07-12 23:43:53 +0200

    Include models for TogetherAI

--- backend/src/Routify.Gateway/Providers/TogetherAi/TogetherAiCompletionProvider.cs
+++ backend/src/Routify.Gateway/Providers/TogetherAi/TogetherAiCompletionProvider.cs
@@ -14,12 +14,121 @@ internal class TogetherAiCompletionProvider(
 {
     private static readonly Dictionary<string, decimal> ModelInputCosts = new()
     {
-        //TODO
+        { "Qwen/Qwen2-72B-Instruct", 0.9m },
+        { "meta-llama/Llama-3-70b-chat-hf", 0.9m },
+        { "Snowflake/snowflake-arctic-instruct", 2.4m },
+        { "meta-llama/Llama-3-8b-chat-hf", 0.2m },
+        { "microsoft/WizardLM-2-8x22B", 1.2m },
+        { "togethercomputer/StripedHyena-Nous-7B", 0.2m },
+        { "databricks/dbrx-instruct", 1.2m },
+        { "allenai/OLMo-7B-Instruct", 0.2m },
+        { "deepseek-ai/deepseek-llm-67b-chat", 0.9m },
+        { "google/gemma-7b-it", 0.2m },
+        { "google/gemma-2b-it", 0.1m },
+        { "NousResearch/Nous-Hermes-2-Mistral-7B-DPO", 0.2m },
+        { "NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT", 0.6m },
+        { "NousResearch/Nous-Hermes-2-Yi-34B", 0.8m },
+        { "codellama/CodeLlama-70b-Instruct-hf", 0.9m },
+        { "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO", 0.6m },
+        { "snorkelai/Snorkel-Mistral-PairRM-DPO", 0.2m },
+        { "deepseek-ai/deepseek-coder-33b-instruct", 0.8m },
+        { "zero-one-ai/Yi-34B-Chat", 0.8m },
+        { "NousResearch/Nous-Hermes-Llama2-13b", 0.3m },
+        { "NousResearch/Nous-Hermes-llama-2-7b", 0.2m },
+        { "togethercomputer/Llama-2-7B-32K-Instruct", 0.2m },
+        { "meta-llama/Llama-2-70b-chat-hf", 0.9m },
+        { "meta-llama/Llama-2-13b-chat-hf", 0.22m },
+        { "meta-llama/Llama-2-7b-chat-hf", 0.2m },
+        { "codellama/CodeLlama-13b-Instruct-hf", 0.22m },
+        { "codellama/CodeLlama-34b-Instruct-hf", 0.78m },
+        { "codellama/CodeLlama-7b-Instruct-hf", 0.2m },
+        { "NousResearch/Nous-Capybara-7B-V1p9", 0.2m },
+        { "teknium/OpenHermes-2p5-Mistral-7B", 0.2m },
+        { "Open-Orca/Mistral-7B-OpenOrca", 0.2m },
+        { "teknium/OpenHermes-2-Mistral-7B", 0.2m },
+        { "Austism/chronos-hermes-13b", 0.3m },
+        { "garage-bAInd/Platypus2-70B-instruct", 0.9m },
+        { "Gryphe/MythoMax-L2-13b", 0.3m },
+        { "togethercomputer/alpaca-7b", 0.2m },
+        { "WizardLM/WizardLM-13B-V1.2", 0.2m },
+        { "upstage/SOLAR-10.7B-Instruct-v1.0", 0.3m },
+        { "OpenAssistant/llama2-70b-oasst-sft-v10", 0.9m },
+        { "openchat/openchat-3.5-1210", 0.2m },
+        { "Qwen/Qwen1.5-7B-Chat", 0.2m },
+        { "Qwen/Qwen1.5-14B-Chat", 0.3m },
+        { "Qwen/Qwen1.5-1.8B-Chat", 0.1m },
+        { "cognitivecomputations/dolphin-2.5-mixtral-8x7b", 0.6m },
+        { "mistralai/Mixtral-8x22B-Instruct-v0.1", 1.2m },
+        { "lmsys/vicuna-13b-v1.5", 0.3m },
+        { "Qwen/Qwen1.5-0.5B-Chat", 0.1m },
+        { "Qwen/Qwen1.5-4B-Chat", 0.1m },
+        { "mistralai/Mistral-7B-Instruct-v0.1", 0.2m },
+        { "mistralai/Mistral-7B-Instruct-v0.2", 0.2m },
+        { "togethercomputer/Pythia-Chat-Base-7B", 0.2m },
+        { "Qwen/Qwen1.5-32B-Chat", 0.8m },
+        { "Qwen/Qwen1.5-72B-Chat", 0.9m },
+        { "mistralai/Mistral-7B-Instruct-v0.3", 0.2m },
+        { "Qwen/Qwen1.5-110B-Chat", 1.8m },
+        { "mistralai/Mixtral-8x7B-Instruct-v0.1", 0.6m },
     };
 
     private static readonly Dictionary<string, decimal> ModelOutputCosts = new()
     {
-        //TODO
+        { "Qwen/Qwen2-72B-Instruct", 0.9m },
+        { "meta-llama/Llama-3-70b-chat-hf", 0.9m },
+        { "Snowflake/snowflake-arctic-instruct", 2.4m },
+        { "meta-llama/Llama-3-8b-chat-hf", 0.2m },
+        { "microsoft/WizardLM-2-8x22B", 1.2m },
+        { "databricks/dbrx-instruct", 1.2m },
+        { "allenai/OLMo-7B-Instruct", 0.2m },
+        { "deepseek-ai/deepseek-llm-67b-chat", 0.9m },
+        { "google/gemma-7b-it", 0.2m },
+        { "google/gemma-2b-it", 0.1m },
+        { "NousResearch/Nous-Hermes-2-Mistral-7B-DPO", 0.2m },
+        { "NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT", 0.6m },
+        { "NousResearch/Nous-Hermes-2-Yi-34B", 0.8m },
+        { "codellama/CodeLlama-70b-Instruct-hf", 0.9m },
+        { "NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO", 0.6m },
+        { "snorkelai/Snorkel-Mistral-PairRM-DPO", 0.2m },
+        { "deepseek-ai/deepseek-coder-33b-instruct", 0.8m },
+        { "zero-one-ai/Yi-34B-Chat", 0.8m },
+        { "NousResearch/Nous-Hermes-Llama2-13b", 0.3m },
+        { "NousResearch/Nous-Hermes-llama-2-7b", 0.2m },
+        { "togethercomputer/Llama-2-7B-32K-Instruct", 0.2m },
+        { "meta-llama/Llama-2-70b-chat-hf", 0.9m },
+        { "meta-llama/Llama-2-13b-chat-hf", 0.22m },
+        { "meta-llama/Llama-2-7b-chat-hf", 0.2m },
+        { "codellama/CodeLlama-13b-Instruct-hf", 0.22m },
+        { "codellama/CodeLlama-34b-Instruct-hf", 0.78m },
+        { "codellama/CodeLlama-7b-Instruct-hf", 0.2m },
+        { "NousResearch/Nous-Capybara-7B-V1p9", 0.2m },
+        { "teknium/OpenHermes-2p5-Mistral-7B", 0.2m },
+        { "Open-Orca/Mistral-7B-OpenOrca", 0.2m },
+        { "teknium/OpenHermes-2-Mistral-7B", 0.2m },
+        { "Austism/chronos-hermes-13b", 0.3m },
+        { "garage-bAInd/Platypus2-70B-instruct", 0.9m },
+        { "Gryphe/MythoMax-L2-13b", 0.3m },
+        { "togethercomputer/alpaca-7b", 0.2m },
+        { "WizardLM/WizardLM-13B-V1.2", 0.2m },
+        { "upstage/SOLAR-10.7B-Instruct-v1.0", 0.3m },
+        { "OpenAssistant/llama2-70b-oasst-sft-v10", 0.9m },
+        { "openchat/openchat-3.5-1210", 0.2m },
+        { "Qwen/Qwen1.5-7B-Chat", 0.2m },
+        { "Qwen/Qwen1.5-14B-Chat", 0.3m },
+        { "Qwen/Qwen1.5-1.8B-Chat", 0.1m },
+        { "cognitivecomputations/dolphin-2.5-mixtral-8x7b", 0.6m },
+        { "mistralai/Mixtral-8x22B-Instruct-v0.1", 1.2m },
+        { "lmsys/vicuna-13b-v1.5", 0.3m },
+        { "Qwen/Qwen1.5-0.5B-Chat", 0.1m },
+        { "Qwen/Qwen1.5-4B-Chat", 0.1m },
+        { "mistralai/Mistral-7B-Instruct-v0.1", 0.2m },
+        { "mistralai/Mistral-7B-Instruct-v0.2", 0.2m },
+        { "togethercomputer/Pythia-Chat-Base-7B", 0.2m },
+        { "Qwen/Qwen1.5-32B-Chat", 0.8m },
+        { "Qwen/Qwen1.5-72B-Chat", 0.9m },
+        { "mistralai/Mistral-7B-Instruct-v0.3", 0.2m },
+        { "Qwen/Qwen1.5-110B-Chat", 1.8m },
+        { "mistralai/Mixtral-8x7B-Instruct-v0.1", 0.6m },
     };
     
     public async Task<CompletionResponse> CompleteAsync(
@@ -117,7 +226,7 @@ internal class TogetherAiCompletionProvider(
     {
         if (ModelInputCosts.TryGetValue(model, out var cost))
         {
-            return cost * tokens;
+            return cost / 1000000 * tokens;
         }
 
         return 0;
@@ -129,7 +238,7 @@ internal class TogetherAiCompletionProvider(
     {
         if (ModelOutputCosts.TryGetValue(model, out var cost))
         {
-            return cost * tokens;
+            return cost / 1000000 * tokens;
         }
 
         return 0;
--- frontend/src/types/providers.ts
+++ frontend/src/types/providers.ts
@@ -137,7 +137,411 @@ export const providers: Provider[] = [
     name: 'TogetherAI',
     description: 'Description coming soon.',
     logo: '/providers/together-ai.png',
-    models: [],
+    models: [
+      {
+        id: 'Qwen/Qwen2-72B-Instruct',
+        name: 'Qwen/Qwen2-72B-Instruct',
+        description:
+          'Qwen2 is the new series of Qwen large language models. For Qwen2, we release a number of base language models and instruction-tuned language models ranging from 0.5 to 72 billion parameters, including a Mixture-of-Experts model.',
+        contextWindow: 131072,
+      },
+      {
+        id: 'meta-llama/Llama-3-70b-chat-hf',
+        name: 'meta-llama/Llama-3-70b-chat-hf',
+        description:
+          'Llama 3 is an auto-regressive language model that uses an optimized transformer architecture. The tuned versions use supervised fine-tuning (SFT) and reinforcement learning with human feedback (RLHF) to align with human preferences for helpfulness and safety.',
+        contextWindow: 8000,
+      },
+      {
+        id: 'Snowflake/snowflake-arctic-instruct',
+        name: 'Snowflake/snowflake-arctic-instruct',
+        description:
+          'Arctic is a dense-MoE Hybrid transformer architecture pre-trained from scratch by the Snowflake AI Research Team',
+        contextWindow: 0,
+      },
+      {
+        id: 'meta-llama/Llama-3-8b-chat-hf',
+        name: 'meta-llama/Llama-3-8b-chat-hf',
+        description:
+          'Llama 3 is an auto-regressive language model that uses an optimized transformer architecture. The tuned versions use supervised fine-tuning (SFT) and reinforcement learning with human feedback (RLHF) to align with human preferences for helpfulness and safety.',
+        contextWindow: 0,
+      },
+      {
+        id: 'microsoft/WizardLM-2-8x22B',
+        name: 'microsoft/WizardLM-2-8x22B',
+        description:
+          "WizardLM-2 8x22B is Wizard's most advanced model, demonstrates highly competitive performance compared to those leading proprietary works and consistently outperforms all the existing state-of-the-art opensource models.",
+        contextWindow: 0,
+      },
+      {
+        id: 'togethercomputer/StripedHyena-Nous-7B',
+        name: 'togethercomputer/StripedHyena-Nous-7B',
+        description:
+          'A hybrid architecture composed of multi-head, grouped-query attention and gated convolutions arranged in Hyena blocks, different from traditional decoder-only Transformers',
+        contextWindow: 0,
+      },
+      {
+        id: 'databricks/dbrx-instruct',
+        name: 'databricks/dbrx-instruct',
+        description:
+          'DBRX Instruct is a mixture-of-experts (MoE) large language model trained from scratch by Databricks. DBRX Instruct specializes in few-turn interactions.',
+        contextWindow: 0,
+      },
+      {
+        id: 'allenai/OLMo-7B-Instruct',
+        name: 'allenai/OLMo-7B-Instruct',
+        description: 'The OLMo models are trained on the Dolma dataset.',
+        contextWindow: 0,
+      },
+      {
+        id: 'deepseek-ai/deepseek-llm-67b-chat',
+        name: 'deepseek-ai/deepseek-llm-67b-chat',
+        description:
+          'trained from scratch on a vast dataset of 2 trillion tokens in both English and Chinese.',
+        contextWindow: 0,
+      },
+      {
+        id: 'google/gemma-7b-it',
+        name: 'google/gemma-7b-it',
+        description:
+          'Gemma is a family of lightweight, state-of-the-art open models from Google, built from the same research and technology used to create the Gemini models.',
+        contextWindow: 0,
+      },
+      {
+        id: 'google/gemma-2b-it',
+        name: 'google/gemma-2b-it',
+        description:
+          'Gemma is a family of lightweight, state-of-the-art open models from Google, built from the same research and technology used to create the Gemini models.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-2-Mistral-7B-DPO',
+        name: 'NousResearch/Nous-Hermes-2-Mistral-7B-DPO',
+        description:
+          "Nous Hermes 2 on Mistral 7B DPO is the new flagship 7B Hermes! This model was DPO'd from Teknium/OpenHermes-2.5-Mistral-7B and has improved across the board on all benchmarks tested - AGIEval, BigBench Reasoning, GPT4All, and TruthfulQA.",
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT',
+        name: 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-SFT',
+        description:
+          'Nous Hermes 2 Mixtral 7bx8 SFT is the new flagship Nous Research model trained over the Mixtral 7bx8 MoE LLM. The model was trained on over 1,000,000 entries of primarily GPT-4 generated data, as well as other high quality data from open datasets across the AI landscape, achieving state of the art performance on a variety of tasks.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-2-Yi-34B',
+        name: 'NousResearch/Nous-Hermes-2-Yi-34B',
+        description:
+          'Nous Hermes 2 - Yi-34B is a state of the art Yi Fine-tune',
+        contextWindow: 0,
+      },
+      {
+        id: 'codellama/CodeLlama-70b-Instruct-hf',
+        name: 'codellama/CodeLlama-70b-Instruct-hf',
+        description:
+          'Code Llama is a family of large language models for code based on Llama 2 providing infilling capabilities, support for large input contexts, and zero-shot instruction following ability for programming tasks.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO',
+        name: 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO',
+        description:
+          'Nous Hermes 2 Mixtral 7bx8 DPO is the new flagship Nous Research model trained over the Mixtral 7bx8 MoE LLM. The model was trained on over 1,000,000 entries of primarily GPT-4 generated data, as well as other high quality data from open datasets across the AI landscape, achieving state of the art performance on a variety of tasks',
+        contextWindow: 0,
+      },
+      {
+        id: 'snorkelai/Snorkel-Mistral-PairRM-DPO',
+        name: 'snorkelai/Snorkel-Mistral-PairRM-DPO',
+        description:
+          'A state-of-the-art model by Snorkel AI, DPO fine-tuned on Mistral-7B.',
+        contextWindow: 0,
+      },
+      {
+        id: 'deepseek-ai/deepseek-coder-33b-instruct',
+        name: 'deepseek-ai/deepseek-coder-33b-instruct',
+        description:
+          'Deepseek Coder is composed of a series of code language models, each trained from scratch on 2T tokens, with a composition of 87% code and 13% natural language in both English and Chinese.',
+        contextWindow: 0,
+      },
+      {
+        id: 'zero-one-ai/Yi-34B-Chat',
+        name: 'zero-one-ai/Yi-34B-Chat',
+        description:
+          'The Yi series models are large language models trained from scratch by developers at 01.AI.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-Llama2-13b',
+        name: 'NousResearch/Nous-Hermes-Llama2-13b',
+        description:
+          'Nous-Hermes-Llama2-13b is a state-of-the-art language model fine-tuned on over 300,000 instructions.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-Llama2-13b',
+        name: 'NousResearch/Nous-Hermes-Llama2-13b',
+        description:
+          'Nous-Hermes-Llama2-13b is a state-of-the-art language model fine-tuned on over 300,000 instructions.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Hermes-llama-2-7b',
+        name: 'NousResearch/Nous-Hermes-llama-2-7b',
+        description:
+          'Nous-Hermes-Llama2-7b is a state-of-the-art language model fine-tuned on over 300,000 instructions.',
+        contextWindow: 0,
+      },
+      {
+        id: 'togethercomputer/Llama-2-7B-32K-Instruct',
+        name: 'togethercomputer/Llama-2-7B-32K-Instruct',
+        description:
+          "Extending LLaMA-2 to 32K context, built with Meta's Position Interpolation and Together AI's data recipe and system optimizations, instruction tuned by Together.",
+        contextWindow: 0,
+      },
+      {
+        id: 'meta-llama/Llama-2-70b-chat-hf',
+        name: 'meta-llama/Llama-2-70b-chat-hf',
+        description:
+          'Llama 2-chat leverages publicly available instruction datasets and over 1 million human annotations. Available in three sizes: 7B, 13B and 70B parameters.',
+        contextWindow: 0,
+      },
+      {
+        id: 'meta-llama/Llama-2-13b-chat-hf',
+        name: 'meta-llama/Llama-2-13b-chat-hf',
+        description:
+          'Llama 2-chat leverages publicly available instruction datasets and over 1 million human annotations. Available in three sizes: 7B, 13B and 70B parameters.',
+        contextWindow: 0,
+      },
+      {
+        id: 'meta-llama/Llama-2-7b-chat-hf',
+        name: 'meta-llama/Llama-2-7b-chat-hf',
+        description:
+          'Llama 2-chat leverages publicly available instruction datasets and over 1 million human annotations. Available in three sizes: 7B, 13B and 70B parameters.',
+        contextWindow: 0,
+      },
+      {
+        id: 'codellama/CodeLlama-13b-Instruct-hf',
+        name: 'codellama/CodeLlama-13b-Instruct-hf',
+        description:
+          'Code Llama is a family of large language models for code based on Llama 2 providing infilling capabilities, support for large input contexts, and zero-shot instruction following ability for programming tasks.',
+        contextWindow: 0,
+      },
+      {
+        id: 'codellama/CodeLlama-34b-Instruct-hf',
+        name: 'codellama/CodeLlama-34b-Instruct-hf',
+        description:
+          'Code Llama is a family of large language models for code based on Llama 2 providing infilling capabilities, support for large input contexts, and zero-shot instruction following ability for programming tasks.',
+        contextWindow: 0,
+      },
+      {
+        id: 'codellama/CodeLlama-7b-Instruct-hf',
+        name: 'codellama/CodeLlama-7b-Instruct-hf',
+        description:
+          'Code Llama is a family of large language models for code based on Llama 2 providing infilling capabilities, support for large input contexts, and zero-shot instruction following ability for programming tasks.',
+        contextWindow: 0,
+      },
+      {
+        id: 'NousResearch/Nous-Capybara-7B-V1p9',
+        name: 'NousResearch/Nous-Capybara-7B-V1p9',
+        description:
+          'first Nous collection of dataset and models made by fine-tuning mostly on data created by Nous in-house',
+        contextWindow: 0,
+      },
+      {
+        id: 'teknium/OpenHermes-2p5-Mistral-7B',
+        name: 'teknium/OpenHermes-2p5-Mistral-7B',
+        description:
+          'Continuation of OpenHermes 2 Mistral model trained on additional code datasets',
+        contextWindow: 0,
+      },
+      {
+        id: 'Open-Orca/Mistral-7B-OpenOrca',
+        name: 'Open-Orca/Mistral-7B-OpenOrca',
+        description:
+          'An OpenOrca dataset fine-tune on top of Mistral 7B by the OpenOrca team.',
+        contextWindow: 0,
+      },
+      {
+        id: 'teknium/OpenHermes-2-Mistral-7B',
+        name: 'teknium/OpenHermes-2-Mistral-7B',
+        description:
+          'State of the art Mistral Fine-tuned on extensive public datasets.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Austism/chronos-hermes-13b',
+        name: 'Austism/chronos-hermes-13b',
+        description:
+          'This model is a 75/25 merge of Chronos (13B) and Nous Hermes (13B) models resulting in having a great ability to produce evocative storywriting and follow a narrative.',
+        contextWindow: 0,
+      },
+      {
+        id: 'garage-bAInd/Platypus2-70B-instruct',
+        name: 'garage-bAInd/Platypus2-70B-instruct',
+        description:
+          'An instruction fine-tuned LLaMA-2 (70B) model by merging Platypus2 (70B) by garage-bAInd and LLaMA-2 Instruct v2 (70B) by upstage.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Gryphe/MythoMax-L2-13b',
+        name: 'Gryphe/MythoMax-L2-13b',
+        description:
+          'MythoLogic-L2 and Huginn merge using a highly experimental tensor type merge technique. The main difference with MythoMix is that I allowed more of Huginn to intermingle with the single tensors located at the front and end of a model.',
+        contextWindow: 0,
+      },
+      {
+        id: 'togethercomputer/alpaca-7b',
+        name: 'togethercomputer/alpaca-7b',
+        description:
+          'Fine-tuned from the LLaMA 7B model on 52K instruction-following demonstrations.',
+        contextWindow: 0,
+      },
+      {
+        id: 'WizardLM/WizardLM-13B-V1.2',
+        name: 'WizardLM/WizardLM-13B-V1.2',
+        description:
+          'This model achieves a substantial and comprehensive improvement on coding, mathematical reasoning and open-domain conversation capacities.',
+        contextWindow: 0,
+      },
+      {
+        id: 'upstage/SOLAR-10.7B-Instruct-v1.0',
+        name: 'upstage/SOLAR-10.7B-Instruct-v1.0',
+        description:
+          'Built on the Llama2 architecture, SOLAR-10.7B incorporates the innovative Upstage Depth Up-Scaling.',
+        contextWindow: 0,
+      },
+      {
+        id: 'OpenAssistant/llama2-70b-oasst-sft-v10',
+        name: 'OpenAssistant/llama2-70b-oasst-sft-v10',
+        description: 'An Open-Assistant fine-tuned model from LLaMA-2 70B.',
+        contextWindow: 0,
+      },
+      {
+        id: 'openchat/openchat-3.5-1210',
+        name: 'openchat/openchat-3.5-1210',
+        description:
+          'A merge of OpenChat 3.5 was trained with C-RLFT on a collection of publicly available high-quality instruction data, with a custom processing pipeline.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-7B-Chat',
+        name: 'Qwen/Qwen1.5-7B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-14B-Chat',
+        name: 'Qwen/Qwen1.5-14B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-1.8B-Chat',
+        name: 'Qwen/Qwen1.5-1.8B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'cognitivecomputations/dolphin-2.5-mixtral-8x7b',
+        name: 'cognitivecomputations/dolphin-2.5-mixtral-8x7b',
+        description:
+          'This Dolphin is really good at coding, I trained with a lot of coding data. It is very obedient but it is not DPO tuned - so you still might need to encourage it in the system prompt as I show in the below examples.',
+        contextWindow: 0,
+      },
+      {
+        id: 'mistralai/Mixtral-8x22B-Instruct-v0.1',
+        name: 'mistralai/Mixtral-8x22B-Instruct-v0.1',
+        description:
+          'The Mixtral-8x22B-Instruct-v0.1 Large Language Model (LLM) is an instruct fine-tuned version of the Mixtral-8x22B-v0.1.',
+        contextWindow: 0,
+      },
+      {
+        id: 'lmsys/vicuna-13b-v1.5',
+        name: 'lmsys/vicuna-13b-v1.5',
+        description:
+          'Vicuna is a chat assistant trained by fine-tuning Llama 2 on user-shared conversations collected from ShareGPT.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-0.5B-Chat',
+        name: 'Qwen/Qwen1.5-0.5B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-0.5B-Chat',
+        name: 'Qwen/Qwen1.5-0.5B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-4B-Chat',
+        name: 'Qwen/Qwen1.5-4B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'mistralai/Mistral-7B-Instruct-v0.1',
+        name: 'mistralai/Mistral-7B-Instruct-v0.1',
+        description: 'instruct fine-tuned version of Mistral-7B-v0.1.',
+        contextWindow: 0,
+      },
+      {
+        id: 'mistralai/Mistral-7B-Instruct-v0.2',
+        name: 'mistralai/Mistral-7B-Instruct-v0.2',
+        description:
+          'The Mistral-7B-Instruct-v0.2 Large Language Model (LLM) is an improved instruct fine-tuned version of Mistral-7B-Instruct-v0.1.',
+        contextWindow: 0,
+      },
+      {
+        id: 'togethercomputer/Pythia-Chat-Base-7B',
+        name: 'togethercomputer/Pythia-Chat-Base-7B',
+        description:
+          'Chat model based on EleutherAI’s Pythia-7B model, and is fine-tuned with data focusing on dialog-style interactions.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-32B-Chat',
+        name: 'Qwen/Qwen1.5-32B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-72B-Chat',
+        name: 'Qwen/Qwen1.5-72B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'mistralai/Mistral-7B-Instruct-v0.3',
+        name: 'mistralai/Mistral-7B-Instruct-v0.3',
+        description:
+          'The Mistral-7B-Instruct-v0.3 Large Language Model (LLM) is an instruct fine-tuned version of the Mistral-7B-v0.3.',
+        contextWindow: 0,
+      },
+      {
+        id: 'Qwen/Qwen1.5-110B-Chat',
+        name: 'Qwen/Qwen1.5-110B-Chat',
+        description:
+          'Qwen1.5 is the beta version of Qwen2, a transformer-based decoder-only language model pretrained on a large amount of data. In comparison with the previous released Qwen.',
+        contextWindow: 0,
+      },
+      {
+        id: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+        name: 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+        description:
+          'The Mixtral-8x7B Large Language Model (LLM) is a pretrained generative Sparse Mixture of Experts.',
+        contextWindow: 0,
+      },
+    ],
   },
   {
     id: 'workers-ai',
