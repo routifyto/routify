4b34041d97ca31455a39780b510f0e5b9cfb5a7d
Hakan Shehu <hakanshehu15@gmail.com>
2024-07-12 19:08:06 +0200

    Fix together ai completion provider

--- backend/src/Routify.Gateway/Providers/TogetherAi/TogetherAiCompletionProvider.cs
+++ backend/src/Routify.Gateway/Providers/TogetherAi/TogetherAiCompletionProvider.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using Routify.Core.Constants;
 using Routify.Gateway.Abstractions;
 using Routify.Gateway.Providers.OpenAi.Models;
+using Routify.Gateway.Providers.TogetherAi.Models;
 
 namespace Routify.Gateway.Providers.TogetherAi;
 
@@ -36,8 +37,57 @@ internal class TogetherAiCompletionProvider(
         var client = httpClientFactory.CreateClient(ProviderIds.TogetherAi);
         client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
 
-        var openAiInput = inputMapper.Map(request.Input);
-        var response = await client.PostAsJsonAsync("chat/completions", openAiInput, cancellationToken);
+        var togetherAiInput = inputMapper.Map(request.Input) as TogetherAiCompletionInput;
+        if (togetherAiInput == null)
+        {
+            return new CompletionResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest,
+            };
+        }
+        
+        if (!string.IsNullOrWhiteSpace(request.Model))
+            togetherAiInput.Model = request.Model;
+
+        if (!request.RouteProviderAttrs.TryGetValue("systemPrompt", out var systemPrompt)
+            && !string.IsNullOrWhiteSpace(systemPrompt))
+        {
+            togetherAiInput.Messages.Insert(0, new TogetherAiCompletionMessageInput
+            {
+                Content = systemPrompt,
+                Role = "system"
+            });
+        }
+
+        if (request.RouteProviderAttrs.TryGetValue("temperature", out var temperatureString) 
+            && !string.IsNullOrWhiteSpace(temperatureString) 
+            && float.TryParse(temperatureString, out var temperature))
+        {
+            togetherAiInput.Temperature = temperature;
+        }
+
+        if (request.RouteProviderAttrs.TryGetValue("maxTokens", out var maxTokensString) 
+            && !string.IsNullOrWhiteSpace(maxTokensString) 
+            && int.TryParse(maxTokensString, out var maxTokens))
+        {
+            togetherAiInput.MaxTokens = maxTokens;
+        }
+
+        if (request.RouteProviderAttrs.TryGetValue("frequencyPenalty", out var frequencyPenaltyString) 
+            && !string.IsNullOrWhiteSpace(frequencyPenaltyString) 
+            && float.TryParse(frequencyPenaltyString, out var frequencyPenalty))
+        {
+            togetherAiInput.FrequencyPenalty = frequencyPenalty;
+        }
+
+        if (request.RouteProviderAttrs.TryGetValue("presencePenalty", out var presencePenaltyString) 
+            && !string.IsNullOrWhiteSpace(presencePenaltyString) 
+            && float.TryParse(presencePenaltyString, out var presencePenalty))
+        {
+            togetherAiInput.PresencePenalty = presencePenalty;
+        }
+        
+        var response = await client.PostAsJsonAsync("chat/completions", togetherAiInput, cancellationToken);
         var responseBody = await response.Content.ReadAsStringAsync(cancellationToken);
         var responseOutput = JsonSerializer.Deserialize<OpenAiCompletionOutput>(responseBody);
 
