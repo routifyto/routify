362a7fe1bc0fd41f06e3a156abcbc6a5cd5a178d
Hakan Shehu <hakanshehu15@gmail.com>
2024-07-23 09:07:15 +0200

    Change approach for dynamic env variables in docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,25 +2,14 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import fs from 'fs';
-import dotenv from 'dotenv';
 
 export default defineConfig(() => {
-  // Load .env file if it exists
-  const envFilePath = './.env';
-  if (fs.existsSync(envFilePath)) {
-    dotenv.config({ path: envFilePath });
-  }
   return {
     plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
-    },
-    define: {
-      VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
-      VITE_GOOGLE_CLIENT_ID: JSON.stringify(process.env.VITE_GOOGLE_CLIENT_ID),
     },
   };
 });
