f3997130aa0e28b900e8bd701f06a1d081041b8c
Hakan Shehu <hakanshehu15@gmail.com>
2024-07-23 08:08:48 +0200

    Inject env variables in runtime as docker entry point for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,14 +2,24 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import dotenv from 'dotenv';
 
 export default defineConfig(() => {
+  // Load .env file if it exists
+  const envFilePath = './.env';
+  if (fs.existsSync(envFilePath)) {
+    dotenv.config({ path: envFilePath });
+  }
   return {
     plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
+    },
+    define: {
+      'process.env': process.env,
     },
   };
 });
