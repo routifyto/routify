370c0e6fdce786f6e0188d29245aa934295f2b82
ylber-gashi <ylberg37@gmail.com>
2024-08-21 00:35:37 +0200

    Added Docker Compose and Helm charts

--- backend/src/Routify.Migrations/Program.cs
+++ backend/src/Routify.Migrations/Program.cs
@@ -1,20 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Routify.Data;
 
-var hostBuilder = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((hostingContext, configuration) =>
+namespace Routify.Migrations;
+
+public class Program
+{
+    public static async Task Main(string[] args)
     {
-        configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true)
-            .AddEnvironmentVariables();  
-    })
-    .ConfigureServices((hostContext, services) =>
-    {
-        services.AddPostgres(hostContext.Configuration);
-    });
-    
-var host = hostBuilder.Build();
-host.Run();
+        var hostBuilder = CreateHostBuilder(args);
+        using var host = hostBuilder.Build();
+
+        var migrationRunner = host.Services.GetRequiredService<MigrationRunner>();
+        await migrationRunner.RunMigrationsAsync();
+
+        Environment.Exit(0);
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json",
+                        optional: true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                var configuration = hostContext.Configuration;
+
+                services.AddPostgres(configuration);
+                services.AddTransient<MigrationRunner>();
+            });
+}
